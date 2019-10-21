2c65f918f71867580f5df6494c80ae2370794b75
karen efremjan <efremyan.kk@phystech.edu>
2019-10-22 00:44:18 +0300

    changes 3 in Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,53 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import './xorovac.jpg'
+import './aman.jpeg'
 function App() {
-  return (
+    /*\ <figure className="figure">
+                        <img src={require("./xorovac.jpg")} className="App-logo" alt="logo.svg" />
+                        <figcaption class="figure-caption">Xorovac</figcaption>
+                    </figure>
+                    <figure className="figure">
+                        <img src={require("./aman.jpeg")} className="App-logo" alt="logo.svg"/>
+                        <figcaption className="figure-caption">Qyabab</figcaption>
+                    </figure>
+                  */
+    return (
       <div className="App">
           <header className="App-header">
               <div className="mt-5">
                   <figure className="figure">
-                      <img src="bludo.png" className="App-logo" alt="logo.svg" />
-                      <figcaption class="figure-caption">Xorovac</figcaption>
+                      <img src="bludo.png" className="App-logo" alt="logo.svg"/>
+                      <figcaption className="figure-caption">Harisa</figcaption>
                   </figure>
                   <figure className="figure">
                       <img src="bludo.png" className="App-logo" alt="logo.svg"/>
-                      <figcaption className="figure-caption">Xorovac</figcaption>
+                      <figcaption className="figure-caption">Lahmajo</figcaption>
                   </figure>
                   <figure className="figure">
                       <img src="bludo.png" className="App-logo" alt="logo.svg"/>
-                      <figcaption className="figure-caption">Xorovac</figcaption>
+                      <figcaption className="figure-caption">Harisa</figcaption>
                   </figure>
                   <figure className="figure">
                       <img src="bludo.png" className="App-logo" alt="logo.svg"/>
-                      <figcaption className="figure-caption">Xorovac</figcaption>
+                      <figcaption className="figure-caption">Lahmajo</figcaption>
                   </figure>
                   <figure className="figure">
                       <img src="bludo.png" className="App-logo" alt="logo.svg"/>
-                      <figcaption className="figure-caption">Xorovac</figcaption>
+                      <figcaption className="figure-caption">Borsh</figcaption>
                   </figure>
                   <figure className="figure">
                       <img src="bludo.png" className="App-logo" alt="logo.svg"/>
-                      <figcaption className="figure-caption">Xorovac</figcaption>
+                      <figcaption className="figure-caption">Dzvadzex</figcaption>
+                  </figure>
+                  <figure className="figure">
+                      <img src="bludo.png" className="App-logo" alt="logo.svg"/>
+                      <figcaption className="figure-caption">Borsh</figcaption>
+                  </figure>
+                  <figure className="figure">
+                      <img src="bludo.png" className="App-logo" alt="logo.svg"/>
+                      <figcaption className="figure-caption">Dzvadzex</figcaption>
                   </figure>
               </div>
               <div className="sticky-top">
